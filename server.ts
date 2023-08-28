d3a04f908d79de82a709bf1a36de33373c4b1d75
Robin Bühler <git+r@obin.ch>
2023-08-28 14:00:24 +0000

    chore: listen on all interfaces

--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ fastify.get('/', async (request, response) => {
   response.type('text/html').send(readFileSync('index.html'))
 })
 
-fastify.listen({ port: 3000 }, function (error) {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, function (error) {
   if (error != null) {
     fastify.log.error(error)
     process.exit(1)
